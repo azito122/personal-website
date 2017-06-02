54b0748180bcc1c87317972e35e40a729b876736
Andy <azito122@gmail.com>
2017-06-02 01:50:34 -0400

    Index and notes

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+</head>
+<body>
+
+<div class="title title_main">
+Andrew Zito
+</div>
+
+<div>
+Who I am
+</div>
+
+<div>
+Stuff I have done
+</div>
+
+</body>
+</html>
