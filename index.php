116a4f15e6c43f26e0b4cae66df29aba5413133c
Andrew Zito <azito122@gmail.com>
2017-07-16 15:52:34 -0400

    Added normalize.css

--- index.php
+++ index.php
@@ -2,26 +2,27 @@
 
 <html>
 <head>
+<link rel="stylesheet" href="style/normalize.css">
 <link rel="stylesheet" href="style/main.css">
 </head>
 <body class="splash">
 
 <section class="section section_title">
-<h1 class="title title_main">
-Andrew Zito
-</h1>
+	<h1 class="title title_main">
+	Andrew Zito
+	</h1>
 </section>
 
 <section class="section section_about">
-<h3 class="title title_section">
-Who I Am
-</h3>
+	<h3 class="title title_section">
+	Who I Am
+	</h3>
 </section>
 
 <section class="section section_projects">
-<h3 class="title title_section">
-Stuff I've Done
-</h3>
+	<h3 class="title title_section">
+	Stuff I've Done
+	</h3>
 </section>
 
 </body>
