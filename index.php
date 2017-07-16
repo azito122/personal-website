9757e0058cf5035183b2a836b70caf3bc3277e1d
Andrew Zito <azito122@gmail.com>
2017-07-16 17:47:23 -0400

    Added a pic, some colors, bit of content

--- index.php
+++ index.php
@@ -8,21 +8,36 @@
 <body class="splash">
 
 <section class="section section_title">
-	<h1 class="title title_main">
-	Andrew Zito
-	</h1>
+	<div class="subsection subsection_left">
+		<div class="img img_headshot"></div>
+	</div>
+
+	<div class="subsection subsection_right">
+		<h1 class="title title_main">
+		Andrew Zito
+		</h1>
+	</div>
 </section>
 
 <section class="section section_about">
-	<h3 class="title title_section">
-	Who I Am
-	</h3>
+	<div class="subsection subsection_left">
+		<h3 class="title title_section">
+		Who I Am
+		</h3>
+	</div>
+
+	<div class="subsection subsection_right">
+		<p class="content content_section"><?php include "content/about.php" ?></p>
+	</div>
 </section>
 
 <section class="section section_projects">
 	<h3 class="title title_section">
 	Stuff I've Done
 	</h3>
+
+	<p class="content content_section">
+	</p>
 </section>
 
 </body>
