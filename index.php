2bdd3efbb85549762c57b64f7d738dc8fbdeb5bf
118498 <u90020617@cbi-bs1.server.lan>
2017-07-27 20:05:51 -0400

    ! Basically starting over

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <html>
 <head>
+
 <link rel="stylesheet" href="style/normalize.css">
 <link rel="stylesheet" href="style/main.css">
 
@@ -10,48 +11,37 @@
   integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
   crossorigin="anonymous"></script>
 <script src="js/main.js"></script>
+
 </head>
-<body class="splash">
 
-<section class="section section_title">
-	<div class="subsection subsection_left">
-		<div class="img img_headshot headshot_01"></div>
-		<div class="img img_headshot headshot_02"></div>
-		<div class="img img_headshot headshot_03"></div>
-		<div class="img img_headshot headshot_04"></div>
-		<div class="img img_headshot headshot_05"></div>
-		<div class="img img_headshot headshot_06"></div>
-		<div class="img img_headshot headshot_07"></div>
-		<div class="img img_headshot headshot_08"></div>
-	</div>
+<body>
 
-	<div class="subsection subsection_right">
-		<h1 class="title title_main">
-		Andrew Zito
-		</h1>
-	</div>
-</section>
+<header>
 
-<section class="section section_about">
-	<div class="subsection subsection_left">
-		<h3 class="title title_section">
-		Who I Am
-		</h3>
-	</div>
+  <table>
+    <tbody>
+      <tr class="first">
+        <td>
+          Andy
+        </td>
+        <td class="dynamic-header-content"></td>
+      </tr>
+      <tr class="second">
+        <td>
+          Codes
+        </td>
+        <td class="dynamic-header-content"></td>
+      </tr>
+      <tr class="third">
+        <td>
+          Things
+        </td>
+        <td class="dynamic-header-content"></td>
+      </tr>
+    </tbody>
+  </table>
 
-	<div class="subsection subsection_right">
-		<p class="content content_section"><?php include "content/about.php" ?></p>
-	</div>
-</section>
-
-<section class="section section_projects">
-	<h3 class="title title_section">
-	Stuff I've Done
-	</h3>
-
-	<p class="content content_section">
-	</p>
-</section>
+</header>
 
 </body>
 </html>
