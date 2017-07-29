c1ab569a44be1db327c347b532e8b97325f0cac1
Andrew Zito <azito122@gmail.com>
2017-07-29 12:35:54 -0400

    More header, added some sections

--- index.php
+++ index.php
@@ -19,34 +19,90 @@
 <header>
 
   <div class="wrapper wrapper-table">
-    <div class="row-first column-first">
-      Andy
-    </div>
-    <div class="dynamic-header-content row-first column-second">
-      (That's me)
-    </div>
-    <div class="row-second column-first">
-      Codes
-    </div>
-    <div class="dynamic-header-content row-second column-second">
-      With
+    <a class="follow" href="#">
+      <div class="row-first column-first">
+        Andy
+      </div>
+      <div class="dynamic-header-content row-first column-second">
+        <span class="follow-text">About me -></span>
+        (That's me)
+      </div>
+    </a>
+    <a class="follow" href="#">
+      <div class="row-second column-first">
+        Codes
+      </div>
+      <div class="dynamic-header-content row-second column-second">
+        <span class="follow-text">Skillsets -></span>
+        With
+          <ul>
+            <li class="visible">JavaScript</li>
+            <li>PHP</li>
+            <li>Python</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+          </ul>
+      </div>
+    </a>
+    <a class="follow" href="#">
+      <div class="row-third column-first">
+        Things
+      </div>
+      <div class="dynamic-header-content row-third column-second">
+        <span class="follow-text">Portfolio -></span>
         <ul>
-          <li class="visible">JavaScript</li>
-          <li>PHP</li>
-          <li>Python</li>
-          <li>HTML5</li>
-          <li>CSS3</li>
-        </ul>
-    </div>
-    <div class="row-third column-first">
-      Things
-    </div>
-    <div class="dynamic-header-content row-third column-second">
-      <span>A syntax tree drawing web app</span>
-    </div>
+          <li>A syntax tree drawing web app</li>
+          <li>A custom Moodle module for temporary enrolments</li>
+          <li>A bit of JavaScript to improve date input</li>
+          <li>A custom Drupal theme for a graduate student's personal website</li>
+          <li>A Python script for evolving English letter frequencies</li>
+      </div>
+    </a>
   </div>
 
 </header>
+
+<section id="about-me">
+  <h1 class="section-title">About Me</h1>
+  <div class="section-content">
+    <p>Hello, my name is Andy Zito. I graduated from Hampshire College in Spring 2017 and am actively seeking a career in web development. Apart from web development, I have academic experience in linguistics. Additionally, I have interests in learning languages, reading books, petting cats, shuffling cards, sleeping, playing video games, changing the world, and building the occasional loft bed.</p>
+  </div>
+</section>
+
+<section id="skillsets">
+  <h1 class="section-title">Skillsets</h1>
+  <div class="section-content">
+
+    <div class="3-col col-1">
+      <h2>Languages</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>PHP</li>
+        <li>Python</li>
+      </ul>
+    </div>
+    <div class="3-col col-2">
+      <h2>Systems/Frameworks</h2>
+      <ul>
+        <li>Git</li>
+        <li>MySQL</li>
+        <li>Whatever</li>
+      </ul>
+    </div>
+
+    <div class="3-col col-3">
+      <h2>Teamwork Tools</h2>
+      <ul>
+        <li>GitLab</li>
+        <li>Slack</li>
+        <li>Trello</li>
+      </ul>
+    </div>
+  </div>
+</section>
+
+<section id="portfolio">
+</section>
 
 </body>
 </html>
