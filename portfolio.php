86dfaf00b270b06144758d402ac0449aaef9c424
Andrew Zito <azito122@gmail.com>
2017-08-04 20:48:00 -0400

    Started working on Portfolio page

--- portfolio.php
+++ portfolio.php
@@ -9,8 +9,44 @@
 <section class="section-1 syntree">
   <h1 class="section-title">Syntree</h1>
   <div cass="section-content">
-    <div class="img portfolio-screenshot"></div>
-    <p class="content">Syntree is a blah blah blah blah blah</p>
+    <div class="portfolio-screenshots">
+      <div class="img portfolio-screenshot first">
+        <span class="screenshot-title">Splash Page</span>
+        <a class="site-link" title="Syntree Splash Page" href="http://syntree.andycodesthings.com/"></a>
+      </div>
+      <div class="img portfolio-screenshot second">
+        <span class="screenshot-title">Main App</span>
+        <a class="site-link" title="Syntree App" href="http://syntree.andycodesthings.com/pages/app-page.php"></a>
+      </div>
+      <div class="img portfolio-screenshot third">
+        <span class="screenshot-title">Docs</span>
+        <a class="site-link" title="Syntree Docs" href="http://syntree.andycodesthings.com/docs/"></a>
+      </div>
+    </div>
+    <div class="technologies-used">JavaScript, PHP, SASS, Git</div>
+    <p class="content">Syntree is a JavaScript based web application that allows users to draw syntax trees using a GUI interface. For more in depth description, see <a href="http://syntree.andycodesthings.com/pages/help.php">the user docs</a> and <a href="http://syntree.andycodesthings.com/docs">the developer docs</a></p>
+  </div>
+</section>
+
+<section class="section-2 syntree">
+  <h1 class="section-title">Syntree</h1>
+  <div cass="section-content">
+    <div class="portfolio-screenshots">
+      <div class="img portfolio-screenshot first">
+        <span class="screenshot-title">Splash Page</span>
+        <a class="site-link" title="Syntree Splash Page" href="http://syntree.andycodesthings.com/"></a>
+      </div>
+      <div class="img portfolio-screenshot second">
+        <span class="screenshot-title">Main App</span>
+        <a class="site-link" title="Syntree App" href="http://syntree.andycodesthings.com/pages/app-page.php"></a>
+      </div>
+      <div class="img portfolio-screenshot third">
+        <span class="screenshot-title">Docs</span>
+        <a class="site-link" title="Syntree Docs" href="http://syntree.andycodesthings.com/docs/"></a>
+      </div>
+    </div>
+    <div class="technologies-used">JavaScript, PHP, SASS, Git</div>
+    <p class="content">Syntree is a JavaScript based web application that allows users to draw syntax trees using a GUI interface. For more in depth description, see <a href="http://syntree.andycodesthings.com/pages/help.php">the user docs</a> and <a href="http://syntree.andycodesthings.com/docs">the developer docs</a></p>
   </div>
 </section>
 
